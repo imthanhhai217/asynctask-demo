ab3a52db3879fbfb1fc96eacb353b3f0b65998e3
jaroid <imthanhhai217@gmail.com>
2022-11-24 20:25:09 +0700

    demo_async

---
+++ app/src/main/java/com/jaroid/asynctask/CalculatorAsyncTask.java
@@ -0,0 +1,11 @@
+package com.jaroid.asynctask;
+
+import android.os.AsyncTask;
+
+public class CalculatorAsyncTask extends AsyncTask<Integer, Integer, Integer> {
+
+    @Override
+    protected Integer doInBackground(Integer... integers) {
+        return null;
+    }
+}
